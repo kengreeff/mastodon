a1ba13803ae9e76089747768dfbd55d5852ae3c8
Claire <claire.github-309c@sitedethib.com>
2023-11-24 17:04:12 +0100

    Change icons to icons optimized for 24×24px

--- app/javascript/mastodon/components/dismissable_banner.tsx
+++ app/javascript/mastodon/components/dismissable_banner.tsx
@@ -8,7 +8,7 @@ import { useCallback, useState, useEffect } from 'react';
 
 import { defineMessages, useIntl } from 'react-intl';
 
-import { ReactComponent as CloseIcon } from 'mastodon/../material-icons/400-20px/close.svg';
+import { ReactComponent as CloseIcon } from 'mastodon/../material-icons/400-24px/close.svg';
 import { changeSetting } from 'mastodon/actions/settings';
 import { bannerSettings } from 'mastodon/settings';
 import { useAppSelector, useAppDispatch } from 'mastodon/store';
